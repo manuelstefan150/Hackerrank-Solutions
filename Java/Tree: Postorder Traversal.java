86523354de52995de06f45dda59e34ddd8ff7965
Manuel Stefan Christopher <43445879+manuelstefan150@users.noreply.github.com>
2020-09-02 00:00:00 +0700

    Create Tree: Postorder Traversal.java

---
+++ Java/Tree: Postorder Traversal.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void postOrder(Node root) {
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<Node> stack = new Stack<Node>();
+        stack.push(root);
+
+        while(!stack.isEmpty()) {
+            Node current = stack.pop();
+            list.add(current.data);
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
+
+        Collections.reverse(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }	
+}
